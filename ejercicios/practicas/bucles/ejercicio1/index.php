c7f22ce06933b10cb1c3d534541a07df6e59ad34
carmonamiguel <carmonamiguel403@gmail.com>
2024-11-10 18:06:56 +0000

    bucle ej1

--- ejercicios/practicas/bucles/ejercicio1/index.php
+++ ejercicios/practicas/bucles/ejercicio1/index.php
@@ -1,15 +1,16 @@
-<!-- 
+<?php 
+
+/**
  *
  *  Números del 1 al 10
  *  @author Miguel Carmona
  * 
- -->
+*/
 
-<?php 
     $num = 0;
     
     for ($num; $num<=10; $num++){
-        echo $num, " ";
+        echo $num.  " ";
     }
 
 ?>

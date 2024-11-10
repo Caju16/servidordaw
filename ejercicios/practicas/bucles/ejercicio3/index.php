9c6229bf2ace3f10b769c7309515703285b9f0bf
carmonamiguel <carmonamiguel403@gmail.com>
2024-11-10 18:16:45 +0000

    bucles ej3

--- ejercicios/practicas/bucles/ejercicio3/index.php
+++ ejercicios/practicas/bucles/ejercicio3/index.php
@@ -1,11 +1,13 @@
-<!-- 
+<?php 
+/**
  *
  *  Tablas de multiplicar
  *  @author Miguel Carmona
  * 
- -->
+*/
 
-<?php 
+
+
    for ($j = 0; $j <= 10; $j++) {
        for ($i = 0; $i <= 10; $i++) {
            echo $i . " x " . $j . " = " . ($i * $j) . "<br>"; 

7196fdfca5688daf84d3d72d93cc9655a012ff54
carmonamiguel <carmonamiguel403@gmail.com>
2024-11-10 17:14:21 +0000

    condicionales ej3

--- ejercicios/practicas/condicionales/ejercicio3/index.php
+++ ejercicios/practicas/condicionales/ejercicio3/index.php
@@ -1,16 +1,22 @@
 <?php
 
-    $fecha = "16/08/2000";
+    // CAMBIANDO EL MÉTODO EN EL QUE SE INTRODUCE LA FECHA (DÍA, MES, AÑO)
 
-    $fecha_nacimiento_convert = DateTime::createFromFormat('d/m/Y', $fecha);
+    // $fecha = "16/08/2000";
+    // $fecha_nacimiento_convert = DateTime::createFromFormat('d/m/Y', $fecha);
+    // $fecha_actual = new DateTime();
+    // echo "Fecha de nacimiento: ", $fecha, "<br>";
+    // $diferencia = $fecha_actual->diff($fecha_nacimiento_convert);
+    // $edad = $diferencia->y;
+    // echo "Tiene ", $edad, " año/s";
+    
 
-    $fecha_actual = new DateTime();
+    // MÉTODO TRADICIONAL (AÑO, MES, DÍA)
 
-    echo "Fecha de nacimiento: ", $fecha, "<br>";
+    $cumpleanos = new DateTime("2000-11-9");
+    $hoy = new DateTime();
+    $annos = $hoy->diff($cumpleanos);
+    echo $annos->y;
 
-    $diferencia = $fecha_actual->diff($fecha_nacimiento_convert);
 
-    $edad = $diferencia->y;
-
-    echo "Tiene ", $edad, " año/s";
 ?>
